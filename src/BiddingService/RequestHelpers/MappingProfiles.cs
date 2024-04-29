3030f392dba22f0937690df699b110a21e9117fe
khoa-tran-39 <ext.khoa.tran@softwareone.com>
2024-04-30 01:17:47 +0700

    bidding services

---
+++ src/BiddingService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Contracts;
+using MassTransit.Caching;
+
+namespace BiddingService;
+
+public class MappingProfiles : Profile
+{
+	public MappingProfiles()
+	{
+		CreateMap<Bid, BidDto>();
+		CreateMap<Bid, BidPlaced>();
+	}
+}
