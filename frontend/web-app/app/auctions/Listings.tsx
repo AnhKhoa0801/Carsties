138696ba617bd4f7599e90600fdabf4b3b85da56
khoa-tran-39 <ext.khoa.tran@softwareone.com>
2024-05-01 03:39:32 +0700

    add state for listing

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -2,28 +2,33 @@
 import React, { useEffect, useState } from "react";
 import AuctionCard from "./AuctionCard";
 import AppPagination from "../components/AppPagination";
-import { Auction, PagedResult } from "@/types";
 import getData from "../actions/auctionActions";
 import Filters from "./Filters";
 import { useParamStore } from "@/hooks/useParamStore";
-import { shallow } from "zustand/shallow";
 import qs from "query-string";
 import EmptyFilter from "../components/EmptyFilter";
+import { useAuctionStore } from "@/hooks/useAuctionStore";
 
 export default function Listings() {
-  const [data, setData] = useState<PagedResult<Auction>>();
-  const params = useParamStore(
-    (state) => ({
-      pageNumber: state.pageNumber,
-      pageSize: state.pageSize,
-      searchTerm: state.searchTerm,
-      orderBy: state.orderBy,
-      filterBy: state.filterBy,
-      seller: state.seller,
-      winner: state.winner,
-    }),
-    shallow
-  );
+  const [loading, setLoading] = useState(true);
+
+  const params = useParamStore((state) => ({
+    pageNumber: state.pageNumber,
+    pageSize: state.pageSize,
+    searchTerm: state.searchTerm,
+    orderBy: state.orderBy,
+    filterBy: state.filterBy,
+    seller: state.seller,
+    winner: state.winner,
+  }));
+
+  const data = useAuctionStore((state) => ({
+    aucions: state.auctions,
+    totalCount: state.totalCount,
+    pageCount: state.pageCount,
+  }));
+
+  const setData = useAuctionStore((state) => state.setData);
 
   const setParams = useParamStore((state) => state.setParams);
   const url = qs.stringifyUrl({ url: "", query: params });
@@ -35,10 +40,11 @@ export default function Listings() {
   useEffect(() => {
     getData(url).then((data) => {
       setData(data);
+      setLoading(false);
     });
   }, [url]);
 
-  if (!data) return <h3>Loading...</h3>;
+  if (loading) return <h3>Loading...</h3>;
 
   if (data.totalCount === 0) return <EmptyFilter showReset />;
 
@@ -50,7 +56,7 @@ export default function Listings() {
       ) : (
         <>
           <div className="grid grid-cols-4 gap-6">
-            {data.results.map((auction) => (
+            {data.aucions.map((auction) => (
               <AuctionCard key={auction.id} auction={auction} />
             ))}
           </div>
